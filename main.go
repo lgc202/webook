bd621770549b290a43bac5f2b3f3b3af8e8b63d2
liguangcai <1299077573@qq.com>
2023-08-28 23:37:04 +0800

    fix cors error

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 	"webook/internal/web"
 )
 
+func corsHandler() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowCredentials: true,
+		// 在使用 JWT 的时候，因为我们使用了 Authorization 的头部，所以要加上
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		// 为了 JWT
+		ExposeHeaders: []string{"X-Jwt-Token"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://") {
+				// 开发环境
+				return true
+			}
+			return strings.Contains(origin, "your_company.com")
+		},
+		MaxAge: 12 * time.Hour,
+	})
+}
+
 func main() {
 	server := gin.Default()
+	//server.Use(corsHandler())
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
 	server.Run(":8080")
